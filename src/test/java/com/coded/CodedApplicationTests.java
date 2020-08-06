063b9c0ea0f65a20f3b99c487f1c6a77a8b423d9
waxx71 <45074900+waxx71@users.noreply.github.com>
2020-08-06 17:43:04 +0800

    Create CodedApplicationTests.java

---
+++ src/test/java/com/coded/CodedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
