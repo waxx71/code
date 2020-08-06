de5919ad35934772ad7071354f136853cdc54fdb
waxx71 <45074900+waxx71@users.noreply.github.com>
2020-08-06 17:36:18 +0800

    Create CodedApplication.java

---
+++ src/main/java/com/coded/CodedApplication.java
@@ -0,0 +1,13 @@
+package com.coded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodedApplication.class, args);
+    }
+
+}
